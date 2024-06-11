32a4cbc87ef32769f358b0b72bae2fe6d7ab71ac
Sernin van de Krol <s.vandekrol@gen25.com>
2024-06-11 17:07:12 +0200

    Add SST deploy

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "nextjs-dashboard",
+      region: "eu-central-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          // Snip
+        }
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;

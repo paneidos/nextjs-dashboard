2b74c86dd7c810b5198f817ecdfb08ecfaf25757
Sernin van de Krol <s.vandekrol@gen25.com>
2024-06-11 17:45:20 +0200

    Insert environment variables

--- sst.config.ts
+++ sst.config.ts
@@ -12,7 +12,16 @@ export default {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         environment: {
-          // Snip
+          POSTGRES_URL: process.env.POSTGRES_URL ?? '',
+          POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL ?? '',
+          POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL ?? '',
+          POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING ?? '',
+          POSTGRES_USER: process.env.POSTGRES_USER ?? '',
+          POSTGRES_HOST: process.env.POSTGRES_HOST ?? '',
+          POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD ?? '',
+          POSTGRES_DATABASE: process.env.POSTGRES_DATABASE ?? '',
+          AUTH_SECRET: process.env.AUTH_SECRET ?? '',
+          AUTH_URL: process.env.AUTH_URL ?? ''
         }
       });
 
